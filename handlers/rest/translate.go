40fc58ba754021e586e839e906706a97178221a9
Adrián Juárez <comecacahuates@yahoo.com>
2025-11-13 13:18:27 -0600

    create translate rest handler

---
+++ handlers/rest/translate.go
@@ -0,0 +1,33 @@
+package rest
+
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+
+	"comecacahuates.com/translation"
+)
+
+type Resp struct {
+	Language    string `json:"language"`
+	Translation string `json:"translation"`
+}
+
+const defaultLanguage = "english"
+
+func TranslateHandler(w http.ResponseWriter, r *http.Request) {
+	enc := json.NewEncoder(w)
+	w.Header().Set("Content-Type", "application/json;charset=utf-8")
+
+	language := defaultLanguage
+	word := strings.ReplaceAll(r.URL.Path, "/", "")
+	translation := translation.Translate(word, language)
+
+	resp := Resp{
+		Language:    language,
+		Translation: translation,
+	}
+	if err := enc.Encode(resp); err != nil {
+		panic("unable to encode response")
+	}
+}
---
+++ handlers/rest/translate_test.go
@@ -0,0 +1,37 @@
+package rest_test
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"comecacahuates.com/handlers/rest"
+)
+
+func TestTranslateAPI(t *testing.T) {
+	// Arrange
+	rr := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/hello", nil)
+
+	handler := http.HandlerFunc(rest.TranslateHandler)
+
+	// Act
+	handler.ServeHTTP(rr, req)
+
+	// Assert
+	if rr.Code != http.StatusOK {
+		t.Errorf(`expected status 200 but received %d`, rr.Code)
+	}
+
+	var resp rest.Resp
+	json.Unmarshal(rr.Body.Bytes(), &resp)
+
+	if resp.Language != "english" {
+		t.Errorf(`expected language "english" but received %s`, resp.Language)
+	}
+
+	if resp.Translation != "hello" {
+		t.Errorf(`expected translation "hello" but received %s`, resp.Translation)
+	}
+}
